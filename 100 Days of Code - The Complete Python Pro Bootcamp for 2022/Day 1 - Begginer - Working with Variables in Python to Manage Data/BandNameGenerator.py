84ff643839a80ae78c62f07195cb05f4271909ed
Goran Šostarko <77921490+gsostarko@users.noreply.github.com>
2022-11-01 13:29:42 +0100

    Add files via upload

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2022/Day 1 - Begginer - Working with Variables in Python to Manage Data/BandNameGenerator.py
@@ -0,0 +1,8 @@
+# 1. Create a greeting for your program 
+print(" Welcome to the Band Name Generator app\n ")
+# 2. Ask the user for the ciry that they grew up in
+city = input("What city did you grow up in?\n")
+# 3. Ask the user for the name of a pet
+pet = input("What is yout pet name?\n")
+# 4. Combine the name of their city and pet and shwo them their band name
+print(f"Yout band name is: {city} {pet}")
---
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2022/Day 1 - Begginer - Working with Variables in Python to Manage Data/print.py
@@ -0,0 +1,14 @@
+### Using `print()` function
+#print("Hello world!")
+
+### Printing in new line with `\n`
+print("Hello world!\nHello world!")
+
+### Concatenating strings (adding strings)
+#print("Hello " + "Goran")
+
+### Python input function (terminal)
+#input("What is yout name?\n")
+
+### Nesting in print function
+print("Hello " + input("What is your name?\n"))
