1f0cc460a9d2ab6ecf6e76883a0c3ad763b2f5c8
Goran Šostarko <77921490+gsostarko@users.noreply.github.com>
2022-11-01 14:42:56 +0100

    Add files via upload

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp for 2022/Day 3 - Begginer - Control Flow and Logical Operators/main.py
@@ -0,0 +1,72 @@
+########################################################################
+# CONTROL FLOW WITH IF/ELSE STATEMENTS AND CONDITIONAL OPERATORS
+########################################################################
+
+#print("Welcome to the rollercoaster!")
+#height = int(input("What is your height in cm?"))
+
+#if height > 120:
+#    print("You can ride the rollercoaster!")
+
+#else:
+#    print("Sorry, you have to grow taler before you can ride.")
+
+########################################################################
+# NESTED IF STATEMENTS AND ELIF STATEMENTS
+########################################################################
+
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm?"))
+
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("What is your age?"))
+
+#     if age < 12:
+#         print("Please pay €5")
+
+#     elif age <= 18:
+#         print("Please pay €7")
+
+#     else:
+#         print("Please pay €12")
+# else:
+#     print("Sorry, you have to grow taler before you can ride.")
+
+
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm?"))
+
+bill = 0
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age?"))
+
+    if age < 12:
+        bill = 5
+        print("¨Child tickets are €5")
+
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are €7")
+
+    else:
+        bill = 12
+        print("Adult tickets are €12")
+
+    want_photo = input("Do your want a photo taken? Y or N.")
+    if want_photo == "Y":
+        #Add €3 to their bill
+        bill += 3
+    
+    print(f"Your final bill is €{bill}")
+
+else:
+    print("Sorry, you have to grow taler before you can ride.")
+
+
+########################################################################
+# LOGICAL OPERATORS
+########################################################################
